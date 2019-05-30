3fae5ad56c5b66c5235e0e80bee2d0346b4472ac
Arjun Variar <arjun.variar@airtel.com>
2019-05-30 19:54:37 +0530

    all changes

--- dataset/freesound_X.py
+++ dataset/freesound_X.py
@@ -7,6 +7,7 @@ import pandas as pd
 from torch.utils.data import Dataset
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
+from torchvision.transforms import *
 
 class TransformTwice:
     def __init__(self, transform):
