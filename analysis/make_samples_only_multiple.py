32e24b7abd402355157c58a41e4065609ab42066
harisankarh <harisankarh@gmail.com>
2019-06-01 11:47:57 +0530

    added analysis notebooks

--- analysis/make_samples_only_multiple.py
+++ analysis/make_samples_only_multiple.py
@@ -9,14 +9,16 @@ train_set = 'train_noisy'
 train_dir = base_dir + train_set + '/'
 train_csv = base_dir + 'processed/' + train_set + '.csv'
 
-output_csv = train_csv + '_sample'
-output_audio_dir = base_dir + 'processed/' + train_set + '_sample/'
+output_csv = train_csv + '_sample_multiple'
+output_audio_dir = base_dir + 'processed/' + train_set + '_sample_multiple/'
 
 lines = open(train_csv).readlines()
 lines = [line.strip() for line in lines]
 
 header = lines[0]
 lines = lines[1:]
+
+lines = [line for line in lines if line.count(",") > 4]
 
 lines_sample = sample(lines, 50)
 
